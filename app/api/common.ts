a392d8691e690d4c1e6f26ec41e69b09d4a809a3
all4fun <wujiwh@gmail.com>
2024-10-12 13:52:16 +0800

    fix: repo url

--- app/api/common.ts
+++ app/api/common.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSideConfig } from "../config/server";
-import { OPENAI_BASE_URL, ServiceProvider } from "../constant";
+import { APP, REPO_URL, OPENAI_BASE_URL, ServiceProvider } from "../constant";
 import { isModelAvailableInServer } from "../utils/model";
 import { cloudflareAIGatewayUrl } from "../utils/cloudflare";
 
@@ -98,6 +98,10 @@ export async function requestOpenai(req: NextRequest) {
       ...(serverConfig.openaiOrgId && {
         "OpenAI-Organization": serverConfig.openaiOrgId,
       }),
+      ...(fetchUrl.startsWith("https://openrouter.ai") && {
+        "HTTP-Referer": `${REPO_URL}`, // Optional, for including your app on openrouter.ai rankings.
+        "X-Title": `${APP}`, // Optional. Shows in rankings on openrouter.ai.
+      }),
     },
     method: req.method,
     body: req.body,
@@ -143,6 +147,7 @@ export async function requestOpenai(req: NextRequest) {
       console.error("[OpenAI] gpt4 filter", e);
     }
   }
+  console.log("[fetch headers]", fetchOptions.headers);
 
   try {
     const res = await fetch(fetchUrl, fetchOptions);
@@ -175,11 +180,6 @@ export async function requestOpenai(req: NextRequest) {
     // Because Vercel uses gzip to compress the response, if we don't remove the content-encoding header
     // The browser will try to decode the response with brotli and fail
     newHeaders.delete("content-encoding");
-
-    // for openrouter activity logs
-    if (fetchUrl.startsWith("https://openrouter.ai")) {
-      newHeaders.set("X-Title", "AIChat");
-    }
 
     return new Response(res.body, {
       status: res.status,
